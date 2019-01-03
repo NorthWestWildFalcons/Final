fd5e79c1b5758cd9cb27d4d3e11de08c6c211941
ChungChainz <44850506+ChungChainz@users.noreply.github.com>
2019-01-03 12:51:07 -0600

    Revert "game 1 update"

--- Final/Final/Game1.Designer.cs
+++ Final/Final/Game1.Designer.cs
@@ -28,102 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnRed = new System.Windows.Forms.Button();
-            this.btnBlue = new System.Windows.Forms.Button();
-            this.btnGreen = new System.Windows.Forms.Button();
-            this.btnYellow = new System.Windows.Forms.Button();
-            this.btnPurple = new System.Windows.Forms.Button();
-            this.lblrandomColor = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // btnRed
-            // 
-            this.btnRed.BackColor = System.Drawing.Color.Red;
-            this.btnRed.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRed.ForeColor = System.Drawing.Color.Black;
-            this.btnRed.Location = new System.Drawing.Point(189, 29);
-            this.btnRed.Name = "btnRed";
-            this.btnRed.Size = new System.Drawing.Size(146, 124);
-            this.btnRed.TabIndex = 0;
-            this.btnRed.Text = "Red";
-            this.btnRed.UseVisualStyleBackColor = false;
-            // 
-            // btnBlue
-            // 
-            this.btnBlue.BackColor = System.Drawing.Color.Blue;
-            this.btnBlue.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBlue.Location = new System.Drawing.Point(431, 29);
-            this.btnBlue.Name = "btnBlue";
-            this.btnBlue.Size = new System.Drawing.Size(146, 124);
-            this.btnBlue.TabIndex = 1;
-            this.btnBlue.Text = "Blue";
-            this.btnBlue.UseVisualStyleBackColor = false;
-            // 
-            // btnGreen
-            // 
-            this.btnGreen.BackColor = System.Drawing.Color.Lime;
-            this.btnGreen.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGreen.Location = new System.Drawing.Point(73, 159);
-            this.btnGreen.Name = "btnGreen";
-            this.btnGreen.Size = new System.Drawing.Size(146, 124);
-            this.btnGreen.TabIndex = 2;
-            this.btnGreen.Text = "Green";
-            this.btnGreen.UseVisualStyleBackColor = false;
-            // 
-            // btnYellow
-            // 
-            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
-            this.btnYellow.Font = new System.Drawing.Font("Showcard Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnYellow.Location = new System.Drawing.Point(551, 159);
-            this.btnYellow.Name = "btnYellow";
-            this.btnYellow.Size = new System.Drawing.Size(146, 124);
-            this.btnYellow.TabIndex = 3;
-            this.btnYellow.Text = "Yellow";
-            this.btnYellow.UseVisualStyleBackColor = false;
-            // 
-            // btnPurple
-            // 
-            this.btnPurple.BackColor = System.Drawing.Color.Purple;
-            this.btnPurple.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPurple.Location = new System.Drawing.Point(311, 159);
-            this.btnPurple.Name = "btnPurple";
-            this.btnPurple.Size = new System.Drawing.Size(146, 124);
-            this.btnPurple.TabIndex = 4;
-            this.btnPurple.Text = "Purple";
-            this.btnPurple.UseVisualStyleBackColor = false;
-            // 
-            // lblrandomColor
-            // 
-            this.lblrandomColor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblrandomColor.Location = new System.Drawing.Point(311, 300);
-            this.lblrandomColor.Name = "lblrandomColor";
-            this.lblrandomColor.Size = new System.Drawing.Size(146, 124);
-            this.lblrandomColor.TabIndex = 5;
-            // 
-            // Game1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblrandomColor);
-            this.Controls.Add(this.btnPurple);
-            this.Controls.Add(this.btnYellow);
-            this.Controls.Add(this.btnGreen);
-            this.Controls.Add(this.btnBlue);
-            this.Controls.Add(this.btnRed);
-            this.Name = "Game1";
             this.Text = "Game1";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnRed;
-        private System.Windows.Forms.Button btnBlue;
-        private System.Windows.Forms.Button btnGreen;
-        private System.Windows.Forms.Button btnYellow;
-        private System.Windows.Forms.Button btnPurple;
-        private System.Windows.Forms.Label lblrandomColor;
     }
 }
